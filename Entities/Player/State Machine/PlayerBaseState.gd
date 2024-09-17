70246ecf84c584d41f1deb969610a480afaf25b8
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-09-17 11:39:32 +0200

    Started on Basics
    - Made a main menu scene, nothing is hooked up yet
    - Made a RD scene with a tileset
    - Made a player Scene
    - Started Work on the player state machine

---
+++ Entities/Player/State Machine/PlayerBaseState.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name PlayerBaseState
+
+func enter(ctx: PlayerStateMachine):
+	pass
+	
+func update(ctx: PlayerStateMachine):
+	pass
+	
+func physics_update(ctx: PlayerStateMachine):
+	pass
+	
+func exit(ctx: PlayerStateMachine):
+	pass
