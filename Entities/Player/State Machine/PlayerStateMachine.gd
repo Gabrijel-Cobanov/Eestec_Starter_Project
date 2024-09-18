96fb423fe6642ee4ada108a140b10e688537583d
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-09-18 11:55:38 +0200

    Added a logo, made player animations
    - Made player animations for idle, attacks, run, and the jump animation

--- Entities/Player/State Machine/PlayerStateMachine.gd
+++ Entities/Player/State Machine/PlayerStateMachine.gd
@@ -6,7 +6,9 @@ signal player_dead
 # Nodes
 @onready var health_component = $"../HealthComponent"
 @onready var CB2D = $".."
-@onready var coyote_timer = $"../Timers/Timer"
+@onready var coyote_timer = $"../Timers/CoyoteTimer"
+@onready var jump_buffer = $"../Timers/JumpBuffer"
+
 
 # States
 @onready var idle = $PlayerIdle
@@ -25,11 +27,13 @@ var nf: float = 500 # normalization factor
 var input_direction: Vector2 = Vector2.ZERO
 var GRAVITY: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var coyote_time: float = 0.2
+var jump_buffer_time: float = 0.1
 @export var movement_speed: float = 10
 @export var jump_force: float = -20
 
 # Flags
 var is_facing_right: bool = true
+var jump_pressed: bool = true
 var can_jump: bool = true
 var should_jump: bool = true
 var can_attack: bool = true
@@ -42,11 +46,16 @@ func _process(delta):
 	
 	if CB2D.is_on_floor():
 		coyote_timer.wait_time = coyote_time
+		jump_buffer.wait_time = jump_buffer_time
 	elif !CB2D.is_on_floor() and coyote_timer.time_left == 0.2:
 		print_debug("Starting timer")
 		coyote_timer.start()
 		
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer.start()
+		
 	can_jump = coyote_timer.time_left >= 0
+	should_jump = jump_buffer.time_left >= 0
 	
 	
 	
@@ -57,7 +66,7 @@ func _physics_process(delta):
 	if !CB2D.is_on_floor():
 		CB2D.velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("jump") and can_jump:
+	if Input.is_action_just_pressed("jump") and can_jump and should_jump:
 		CB2D.velocity.y =  jump_force * 50
 		
 	CB2D.velocity.x = input_direction.x * movement_speed * nf * delta
